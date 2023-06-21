6aedd9c17fbd31b959b7819c2105f7d1c9ccd0ba
LaithShatnawi <laith.sh66@gmail.com>
2023-06-21 17:50:35 +0000

    finishing with the lab

---
+++ src/auth/middleware/basic.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const base64 = require('base-64');
+const { users } = require('../models/index');
+
+module.exports = async (req, res, next) => {
+
+  if (!req.headers.authorization) { return _authError(); }
+
+  let basic = req.headers.authorization.split(" ");
+  let encodedValue = basic.pop();
+  let [username, pass] = base64.decode(encodedValue).split(':');
+
+  try {
+    req.user = await users.authenticateBasic(username, pass)
+    next();
+  } catch (e) {
+    console.log(e);
+    res.status(403).send('Invalid Login');
+  }
+
+}
